3a721ab21a36ac9d0e46db335bd1ffa12627e73c
nimit0703 <nimit.190410107143@gmail.com>
2023-10-04 15:51:30 +0530

    split forms

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,9 +1,7 @@
 <template>
   <!-- Auth Modal -->
   <div class="fixed z-10 inset-0 overflow-y-auto" id="modal" :class="hiddenClass">
-    <div
-      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-    >
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
       <div class="fixed inset-0 transition-opacity">
         <div class="absolute inset-0 bg-gray-800 opacity-75"></div>
       </div>
@@ -11,9 +9,7 @@
       <!-- This element is to trick the browser into centering the modal contents. -->
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
 
-      <div
-        class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-      >
+      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
         <!-- Add margin if you want to see some of the overlay behind the modal-->
         <div class="py-4 text-left px-6">
           <!--Title-->
@@ -54,124 +50,9 @@
           </ul>
 
           <!-- Login Form -->
-          <form v-show="tab === 'login'">
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <input
-                type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-              />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <input
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
-            >
-              Submit
-            </button>
-          </form>
+          <login-form v-show="tab === 'login'"></login-form>
           <!-- Registration Form -->
-          <div class="text-white text-center font-bold p-4 rounded mb-4" v-if="reg_show_alert"
-          :class="reg_alert_varient"
-          >{{ reg_alert_msg }}</div>
-          <vee-form
-            v-show="tab === 'register'"
-            :validation-schema="schema"
-            @submit="register"
-            :initial-values="userData" >
-            <!-- Name -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Name</label>
-              <vee-field
-                name="name"
-                type="text"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name"
-              />
-              <ErrorMessage class="text-red-600" name="name" />
-            </div>
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <vee-field
-                name="email"
-                type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-              />
-              <ErrorMessage class="text-red-600" name="email" />
-            </div>
-            <!-- Age -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Age</label>
-              <vee-field
-                name="age"
-                type="number"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-              />
-              <ErrorMessage class="text-red-600" name="age" />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <vee-field
-                name="password"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-                :bails="false"
-              />
-              <ErrorMessage class="text-red-600" name="password" />
-            </div>
-            <!-- Confirm Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Confirm Password</label>
-              <vee-field
-                name="confirmed_password"
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password"
-              />
-              <ErrorMessage class="text-red-600" name="confirmed_password" />
-            </div>
-            <!-- Country -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Country</label>
-              <vee-field
-                as="select"
-                name="country"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-              >
-                <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Germany">Germany</option>
-                <option value="Canada">Canada</option>
-              </vee-field>
-              <ErrorMessage class="text-red-600" name="country" />
-            </div>
-            <!-- TOS -->
-            <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-              <label class="inline-block">Accept terms of service</label>
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
-              :disabled="reg_in_submission"
-              >
-              Submit
-            </button>
-          </vee-form>
+          <register-form v-show="tab === 'register'"></register-form>
         </div>
       </div>
     </div>
@@ -181,28 +62,18 @@
 <script lang="ts">
 import { mapState, mapWritableState } from 'pinia'
 import useModalStore from '@/stores/modal'
+import RegisterForm from './RegisterForm.vue'
+import LoginForm from './LoginForm.vue'
 
 export default {
   name: 'Auth',
+  components: {
+    RegisterForm,
+    LoginForm
+  },
   data() {
     return {
-      tab: 'login',
-      schema: {
-        name: 'required|min:3|max:25|alpha_spaces',
-        email: 'required|email',
-        age: 'required|min_value:5|max_value:135',
-        password: 'required|min:3|max:25',
-        confirmed_password: 'confirmed:@password',
-        country: 'required|not_one_of:Canada',
-        tos: ''
-      },
-      userData: {
-        country: 'USA'
-      },
-      reg_in_submission: false,
-      reg_show_alert: false,
-      reg_alert_varient: 'bg-blue-500',
-      reg_alert_msg: 'Please wait! your acount is being creating'
+      tab: 'login'
     }
   },
   computed: {
@@ -211,14 +82,6 @@ export default {
       modelVisiblity: 'isOpen'
     })
   },
-  methods: {
-    register(values: object) {
-      this.reg_show_alert=true;
-      this.reg_in_submission=true;
-      this.reg_alert_varient="bg-green-500";
-      this.reg_alert_msg = "done";
-      console.log(values);
-    }
-  }
+  methods: {}
 }
 </script>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,64 @@
+<template>
+   <form >
+            <!-- Email -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Email</label>
+              <input
+                type="email"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Enter Email"
+              />
+            </div>
+            <!-- Password -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Password</label>
+              <input
+                type="password"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Password"
+              />
+            </div>
+            <button
+              type="submit"
+              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+            >
+              Submit
+            </button>
+          </form>
+</template>
+<script lang="ts">
+
+
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      schema: {
+        name: 'required|min:3|max:25|alpha_spaces',
+        email: 'required|email',
+        age: 'required|min_value:5|max_value:135',
+        password: 'required|min:3|max:25',
+        confirmed_password: 'confirmed:@password',
+        country: 'required|not_one_of:Canada',
+        tos: ''
+      },
+      userData: {
+        country: 'USA'
+      },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_varient: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! your acount is being creating'
+    }
+  },
+  methods: {
+    register(values: object) {
+      this.reg_show_alert = true
+      this.reg_in_submission = true
+      this.reg_alert_varient = 'bg-green-500'
+      this.reg_alert_msg = 'done'
+      console.log(values)
+    }
+  }
+}
+</script>
